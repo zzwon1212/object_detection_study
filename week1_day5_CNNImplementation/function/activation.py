a958adbb5881475f86fd7dbf78fd24cf49530627
zzwon1212 <zzwon1212@gmail.com>
2023-12-23 11:54:35 +0900

    Add backpropagation using NumPy in CNN
    Lecture: CNN - Practice 2

--- week1_day5_CNNImplementation/function/activation.py
+++ week1_day5_CNNImplementation/function/activation.py
@@ -3,27 +3,27 @@ import numpy as np
 def relu(x):
   x_shape = x.shape
   x = np.reshape(x, [-1])
-  x = [max(v, 0) for v in x]
+  x = np.array([max(v, 0) for v in x], dtype=np.float32)
   x = np.reshape(x, x_shape)
   return x
 
 def leaky_relu(x):
   x_shape = x.shape
   x = np.reshape(x, [-1])
-  x = [max(v, v*0.1) for v in x]
+  x = np.array([max(v, v*0.1) for v in x], dtype=np.float32)
   x = np.reshape(x, x_shape)
   return x
 
 def sigmoid(x):
   x_shape = x.shape
   x = np.reshape(x, [-1])
-  x = [1 / (1 + np.exp(-v)) for v in x]
+  x = np.array([1 / (1 + np.exp(-v)) for v in x], dtype=np.float32)
   x = np.reshape(x, x_shape)
   return x
 
 def tanh(x):
   x_shape = x.shape
   x = np.reshape(x, [-1])
-  x = [np.tanh(v) for v in x]
+  x = np.array([np.tanh(v) for v in x], dtype=np.float32)
   x = np.reshape(x, x_shape)
   return x
--- week1_day5_CNNImplementation/main.py
+++ week1_day5_CNNImplementation/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import time
+import sys
 
 import numpy as np
 import torch
@@ -10,6 +11,8 @@ from function.convolution import Conv
 from function.pooling import Pool
 from function.fc import FC
 from function.activation import *
+
+np.random.seed(12)
 
 def convolution():
   # define the shape of input & weight
@@ -129,7 +132,114 @@ def plot_activation():
   plt.legend()
   plt.savefig("activation_graph.png")
 
+def shallow_network():
+  # input: [1, 1, 6, 6], iter: 2
+  X = [np.random.standard_normal([1, 1, 6, 6]),
+       np.random.standard_normal([1, 1, 6, 6])]
+  Y = np.array([1, 1], dtype=np.float32)
+
+  # Conv weights, [1, 1, 3, 3], [1, 4]
+  W1 = np.random.standard_normal([1, 1, 3, 3])
+  W2 = np.random.standard_normal([1, 4])
+
+  padding = 0
+  stride = 1
+
+  # Shape of L1 layer
+  L1_h = (X[0].shape[2] + 2*padding - W1.shape[2]) // stride + 1
+  L1_w = (X[0].shape[3] + 2*padding - W1.shape[3]) // stride + 1
+
+  # Conv
+  Convolution = Conv(batch=X[0].shape[0],
+                     in_c=X[0].shape[1],
+                     out_c=W1.shape[0],
+                     in_h=X[0].shape[2],
+                     in_w=X[0].shape[3],
+                     k_h=W1.shape[2],
+                     k_w=W1.shape[3],
+                     dilation=1,
+                     stride=stride,
+                     padding=padding)
+
+  # Conv diff
+  Conv_diff = Conv(batch=X[0].shape[0],
+                   in_c=X[0].shape[1],
+                   out_c=W1.shape[0],
+                   in_h=X[0].shape[2],
+                   in_w=X[0].shape[3],
+                   k_h=L1_h,
+                   k_w=L1_w,
+                   dilation=1,
+                   stride=1,
+                   padding=0)
+
+  # Max Pooling
+  Pooling = Pool(batch=X[0].shape[0],
+                 in_c=W1.shape[0],
+                 out_c=W1.shape[0],
+                 in_h=L1_h,
+                 in_w=L1_w,
+                 kernel=2,
+                 dilation=1,
+                 stride=2,
+                 padding=0)
+
+  # FC
+  Fc = FC(batch=X[0].shape[0])
+
+  num_epoch = 100
+  for e in range(num_epoch):
+    total_loss = 0
+    for i in range(len(X)):
+      # Forward
+      L1 = Convolution.gemm(X[i], W1)
+      L1_a = sigmoid(L1)
+      L1_MAX = Pooling.pool(L1_a)
+
+      L2 = Fc.fc(L1_MAX, W2)
+      L2_a = sigmoid(L2)
+
+      loss = np.square(Y[i] - L2_a) * 0.5
+
+      total_loss += loss
+
+      # Backward
+      # 1. dE / dW2
+      diff_E__L2_a = L2_a - Y[i]
+
+      diff_L2_a__L2 = L2_a * (1 - L2_a)
+
+      diff_L2__W2 = L1_MAX.reshape([1, -1])
+
+      diff_E__W2 = diff_E__L2_a * diff_L2_a__L2 * diff_L2__W2
+
+      # 2. dE / dW1
+      diff_E__L2_a = L2_a - Y[i]
+
+      diff_L2_a__L2 = L2_a * (1 - L2_a)
+
+      diff_L2__L1_MAX = W2.reshape([1, 1, 2, 2])
+      diff_L2__L1_MAX = diff_L2__L1_MAX.repeat(2, axis=2).repeat(2, axis=3)
+
+      L1_MAX_repeat = L1_MAX.repeat(2, axis=2).repeat(2, axis=3)
+      diff_L1_MAX__L1_a = np.equal(L1_MAX_repeat, L1_a).astype(int)
+
+      diff_L1_a__L1 = L1_a * (1 - L1_a)
+
+      diff_L1__w1 = X[i]
+
+      diff_E__W1 = diff_E__L2_a * diff_L2_a__L2 * diff_L2__L1_MAX * diff_L1_MAX__L1_a * diff_L1_a__L1
+      diff_E__W1 = Conv_diff.gemm(diff_L1__w1, diff_E__W1)
+      # print("diff_E__W1\n", diff_E__W1)
+
+      # Update
+      W2 = W2 - 0.01 * diff_E__W2
+      W1 = W1 - 0.01 * diff_E__W1
+
+    print(f"{e} epoch loss {total_loss / len(X)}")
+
 if __name__ == "__main__":
   # convolution()
-  forward_net()
-  plot_activation()
+  # forward_net()
+  # plot_activation()
+  shallow_network()
